dadbfa158596aba29470d68f6f7289dda2bf916f
RaulDC <81639203+RaulDC@users.noreply.github.com>
2023-10-16 11:52:46 -0500

    actualización de credenciales para clever cloud

--- connection.py
+++ connection.py
@@ -3,9 +3,9 @@ import mysql.connector
 class DataBase:
     def __init__(self):
         self.connection = mysql.connector.connect(
-            host='localhost',
-            database='bd_certus',                             
-            user='root',                                      
-            password='72806558'
+            host='bbdyxtq2xayjozsfil1n-mysql.services.clever-cloud.com',
+            database='bbdyxtq2xayjozsfil1n',                             
+            user='u4wtydnovodwvldv',                                      
+            password='oNdjuw7Jl28J3A26x0U3'
             )
         self.cursor=self.connection.cursor()
